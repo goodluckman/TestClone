0817ec97a98fa2e7f3fec867f30cb2853dd0801e
YinZ_global <YinZ_global@qq.com>
2018-11-30 10:22:14 +0800

    test git pull

--- src/TestClone.java
+++ src/TestClone.java
@@ -4,6 +4,7 @@ public class TestClone {
 	public static void main(String[] args) {
 		System.out.println("welcome to Git!");
 		System.out.println("modified by UserB");
+		System.out.println("test git pull");
 	}
 
 }
